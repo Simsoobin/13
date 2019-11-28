df1907f2b586c39637313b885270c143cb56fe25
Simsoobin <55381009+Simsoobin@users.noreply.github.com>
2019-11-28 15:44:43 +0900

    13_2

--- main.c
+++ main.c
@@ -2,21 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-struct student {
-	int		ID;
-	char	name[10];
-	double	grade;
-};
-int main(int argc, char *argv[]) {
-	struct student s1 = { 24, "soo bin", 4.3 
-	};
-	s1.ID =1711392;
-	strcpy(s1.name, "soobin");
-	s1.grade=4.2;
-	 
-	 printf("ID : %d\n", s1.ID);
-	 printf("name : %s\n", s1.name);
-	 printf("grade : %f\n", s1.grade);
 
+int main(int argc, char *argv[]) {
+		
+	FILE *fp;
+	char filepath[100];
+	
+	//file open
+	printf("input the file path : ");
+	scanf("%s", filepath);
+	fp = fopen(filepath, "r");
+	
+	if (fp ==NULL)
+	{
+		printf("file path is wrong! %s\n", filepath);
+		return -1;
+	}
+	fclose(fp);
+	//word reading &analysis
 	return 0;
 }
